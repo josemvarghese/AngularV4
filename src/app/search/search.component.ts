1cbe07a06175adf0a5ca0caeb17a59e55a644f73
jose <jose@voris.co.in>
2017-05-24 16:46:41 +0530

    angular 4 ngform basics

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  hiddenValue = 'this is hidden value';
   constructor() { }
 
   ngOnInit() {
   }
-
+  submitQuery(event,formData){
+    console.log(event);
+    console.log(formData.value);
+  }
+  searchQueryChange(){
+    this.hiddenValue='New hidden value';
+  }
 }
