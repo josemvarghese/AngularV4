65a09e7a2e173820090e6f950501de8be1d830f1
jose <jose@voris.co.in>
2017-05-24 12:30:07 +0530

    video details page - filter - angular4 done

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit,OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {Http } from '@angular/http'
 
 @Component({
   selector: 'app-video-detail',
   templateUrl: './video-detail.component.html',
   styleUrls: ['./video-detail.component.css']
 })
-export class VideoDetailComponent implements OnInit {
+export class VideoDetailComponent implements OnInit,OnDestroy {
   private routeSub:any;
   id:number;
-  constructor(private route:ActivatedRoute) { }
+  private request:any;
+  videoObj:any;
+  title:string;
+  text:string;
+
+  constructor(private route:ActivatedRoute,private http:Http) { }
 
   ngOnInit() {
     this.routeSub = this.route.params.subscribe(params => {
@@ -17,12 +23,22 @@ export class VideoDetailComponent implements OnInit {
       console.log(params.id);
       console.log(this.id);
       console.log(window);
-
+      this.request = this.http.get('assets/json/videos.json').subscribe(data=>{
+        data.json().vlist.filter(item=>{
+          // console.log(item);
+          if(item.id == this.id){
+            this.videoObj = item;
+          }
+        })
+      })
 
     })
   }
   ngOnDestroy(){
     this.routeSub.unsubscribe();
+    this.request.unsubscribe();
   }
-
+  getEmbedUrl(item){
+    return  'https://www.youtube.com/embed/'+item.embed;
+  }
 }
